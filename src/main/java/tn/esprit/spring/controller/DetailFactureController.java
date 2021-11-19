f90e4a659436d6687da85df6c12662eacacaa027
karim369 <karim.chakroun@outlook.com>
2021-11-19 21:26:54 +0100

    sw

--- src/main/java/tn/esprit/spring/controller/DetailFactureController.java
+++ src/main/java/tn/esprit/spring/controller/DetailFactureController.java
@@ -5,10 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import tn.esprit.spring.entity.DetailFacture;
 import tn.esprit.spring.service.DetailFactureServiceImpl;
 
+@RestController
+@RequestMapping("/DetailFacture")
 public class DetailFactureController {
 	
 	@Autowired
--- src/main/java/tn/esprit/spring/controller/FournisseurController.java
+++ src/main/java/tn/esprit/spring/controller/FournisseurController.java
@@ -7,10 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import tn.esprit.spring.entity.Founisseur;
 import tn.esprit.spring.service.FournisseurServiceImpl;
 
+@RestController
+@RequestMapping("/Fournisseur")
 public class FournisseurController {
 	
 	
